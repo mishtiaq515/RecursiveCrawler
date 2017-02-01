96cec4b3a3b0df67dedc2ab9f98fd10b34447cf8
Muhammad Ishtiaq <muhammad.ishtiaq@arbisoft.com>
2017-02-01 11:39:30 +0500

    refactored to specify to download_delay from constants

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 
-site_url = "http://www.google.com"
+SITE_URL = "http://www.google.com"
+MAX_CONCURRENT_REQUESTS = 10
+MAX_PAGES_TO_CRAWL = 10
+DOWNLOAD_DELAY = 3  # seconds
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from multiprocessing import freeze_support
-
 from crawler import Crawler, ThreadScheduler
-from constants import site_url
+from constants import SITE_URL, MAX_PAGES_TO_CRAWL, MAX_CONCURRENT_REQUESTS, DOWNLOAD_DELAY
 
 
 if __name__ == "__main__":
-    freeze_support()
-    crawler = Crawler(base_url=site_url)
-    thread_scheduler = ThreadScheduler(crawler=crawler)
+    crawler = Crawler(base_url=SITE_URL, max_pages_to_crawl=MAX_PAGES_TO_CRAWL)
+    thread_scheduler = ThreadScheduler(crawler=crawler, max_concurrent_requests=MAX_CONCURRENT_REQUESTS,
+                                       download_delay=DOWNLOAD_DELAY)
     thread_scheduler.run()
 
     print("Page visited: %s, Bytes downloaded: %s"
